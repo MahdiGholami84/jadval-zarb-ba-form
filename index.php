9f8c731ad1d3dde9bfffbbb284ee293c7255efb3
Mahdi <mahdinomad75@gmail.com>
2022-11-29 09:04:49 +0330

    دو ورودی کردن جدول ضرب

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <?php
     if (isset($_POST ['submit'])) {
         $n1 = $_POST['n1'];
-
+        $n2 = $_POST['n2'];
         for ($x = 1; $x <= $n1; $x++) {
             echo "<tr>";
-            for ($y = 1; $y <= $n1; $y++) {
+            for ($y = 1; $y <= $n2; $y++) {
                 echo "<td>";
                 echo $x * $y . "</br>";
                 echo "</td>";
@@ -19,8 +19,10 @@
     ?>
 </table>
 <form method="post">
-    <label>number:</label><br/>
+    <label>number1:</label><br/>
     <input type="number" name="n1"/><br/>
+    <label>number2:</label><br/>
+    <input type="number" name="n2"/><br/>
     <input type="submit" name="submit" value="run"/>
 </form>
 </body>
